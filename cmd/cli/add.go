97407870f6ca7d444a384a99909cc65adb402406
denesbeck <denes.beck1@outlook.com>
2025-10-14 19:51:50 +0200

    refactor: return code, test names

--- cmd/cli/add.go
+++ cmd/cli/add.go
@@ -24,8 +24,8 @@ var addCmd = &cobra.Command{
 func runAddCommand(filePath string) int {
 	initialized := IsInitialized()
 	if !initialized {
-		color.Red(COMMON_RETURN_CODES[99])
-		return 99
+		color.Red(COMMON_RETURN_CODES[001])
+		return 001
 	}
 
 	_, fileName := ParsePath(filePath)
@@ -38,17 +38,17 @@ func runAddCommand(filePath string) int {
 		if added {
 			if !exists {
 				removeFileAndLog(id, "added")
-				color.Cyan(ADD_RETURN_CODES[1])
-				return 1
+				color.Cyan(ADD_RETURN_CODES[101])
+				return 101
 			}
 			modified := IsModified(filePath, dirs.StagingAdded+id+"/"+fileName)
 			if modified {
 				AddToStaging(id, filePath, "added")
-				color.Cyan(ADD_RETURN_CODES[2])
-				return 2
+				color.Cyan(ADD_RETURN_CODES[102])
+				return 102
 			}
-			color.Cyan(ADD_RETURN_CODES[3])
-			return 3
+			color.Cyan(ADD_RETURN_CODES[103])
+			return 103
 		}
 		modified, id, _ := LogEntryLookup("MOD", filePath)
 		if modified {
@@ -60,16 +60,16 @@ func runAddCommand(filePath string) int {
 				                * Therefore calling LogOperation() and skipping AddToStaging().
 				*/
 				LogOperation(generatedId, "REM", filePath)
-				return 4
+				return 104
 			}
 			modified := IsModified(filePath, dirs.StagingModified+id+"/"+fileName)
 			if modified {
 				AddToStaging(id, filePath, "modified")
-				color.Cyan(ADD_RETURN_CODES[5])
-				return 5
+				color.Cyan(ADD_RETURN_CODES[105])
+				return 105
 			}
-			color.Cyan(ADD_RETURN_CODES[6])
-			return 6
+			color.Cyan(ADD_RETURN_CODES[106])
+			return 106
 		}
 		removed, id, _ := LogEntryLookup("REM", filePath)
 		if removed {
@@ -80,15 +80,15 @@ func runAddCommand(filePath string) int {
 				modified := IsModified(filePath, dirs.Commits+commitId+"/"+fileId+"/"+fileName)
 				if modified {
 					stageAndLog(generatedId, filePath, "modified")
-					color.Cyan(ADD_RETURN_CODES[7])
-					return 7
+					color.Cyan(ADD_RETURN_CODES[107])
+					return 107
 				}
 				/*
 				 If file was staged (REM) and added back to workdir without any modifications, everything should be unchanged.
 				*/
 			} else {
-				color.Cyan(ADD_RETURN_CODES[8])
-				return 8
+				color.Cyan(ADD_RETURN_CODES[108])
+				return 108
 			}
 		}
 	} else {
@@ -97,26 +97,26 @@ func runAddCommand(filePath string) int {
 		if isDeleted {
 			AddToStaging(generatedId, dirs.Commits+commitId+"/"+fileId+"/"+fileName, "removed")
 			LogOperation(generatedId, "REM", filePath)
-			color.Cyan(ADD_RETURN_CODES[9])
-			return 9
+			color.Cyan(ADD_RETURN_CODES[109])
+			return 109
 		}
 
 		if isCommitted {
 			modified := IsModified(filePath, dirs.Commits+commitId+"/"+fileId+"/"+fileName)
 			if modified {
 				stageAndLog(generatedId, filePath, "modified")
-				return 10
+				return 110
 			} else {
-				color.Red(ADD_RETURN_CODES[11])
-				return 11
+				color.Red(ADD_RETURN_CODES[111])
+				return 111
 			}
 		} else {
 			stageAndLog(generatedId, filePath, "added")
-			color.Cyan(ADD_RETURN_CODES[12])
-			return 12
+			color.Cyan(ADD_RETURN_CODES[112])
+			return 112
 		}
 	}
-	return 0
+	return 100
 }
 
 func removeFileAndLog(id string, op string) {
--- cmd/cli/return_codes.go
+++ cmd/cli/return_codes.go
@@ -1,20 +1,20 @@
 package cli
 
 var COMMON_RETURN_CODES = map[int]string{
-	99: "CSync not initialized",
+	001: "CSync not initialized",
 }
 
 var ADD_RETURN_CODES = map[int]string{
-	1:  "File removed from staging", // file was staged (ADD), but it got removed
-	2:  "Staged file updated",       // file was staged (ADD), but it got modified
-	3:  "File already staged",       // the user staged the same file again (ADD)
-	4:  "",                          // file was staged (MOD), but it got removed
-	5:  "Staged file updated",       // file was staged (MOD), but it got modified
-	6:  "File already staged",       // the user staged the same file again (MOD)
-	7:  "Staged file update",        // file was staged (REM), but it got added back and modified
-	8:  "File already staged",       // the user staged the same file again (REM)
-	9:  "Filed added to staging",    // file committed but not staged -> staged (REM)
-	10: "Filed added to staging",    // file committed but not staged -> staged (MOD)
-	11: "File not modified",         // file committed, not staged, not modified
-	12: "File added to staging",     // file not committed, not staged -> staged (ADD)
+	101: "File removed from staging", // file was staged (ADD), but it got removed
+	102: "Staged file updated",       // file was staged (ADD), but it got modified
+	103: "File already staged",       // the user staged the same file again (ADD)
+	104: "",                          // file was staged (MOD), but it got removed
+	105: "Staged file updated",       // file was staged (MOD), but it got modified
+	106: "File already staged",       // the user staged the same file again (MOD)
+	107: "Staged file update",        // file was staged (REM), but it got added back and modified
+	108: "File already staged",       // the user staged the same file again (REM)
+	109: "Filed added to staging",    // file committed but not staged -> staged (REM)
+	110: "Filed added to staging",    // file committed but not staged -> staged (MOD)
+	111: "File not modified",         // file committed, not staged, not modified
+	112: "File added to staging",     // file not committed, not staged -> staged (ADD)
 }
