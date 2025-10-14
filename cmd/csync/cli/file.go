e52086b41be708730cf1e73c85add09e2cef0c61
denesbeck <denes.beck1@outlook.com>
2025-10-14 19:51:50 +0200

    feat: empty dir

--- cmd/csync/cli/file.go
+++ cmd/csync/cli/file.go
@@ -1,27 +1,27 @@
 package cli
 
 import (
-	"errors"
+	"bufio"
 	"io"
 	"log"
 	"os"
 )
 
-func CopyFile(src, dst string) (int64, error) {
+func CopyFile(src, dst string) {
 	// File exists?
 	sourceFileStat, err := os.Stat(src)
 	if err != nil {
-		return 0, err
+		log.Fatal("Source file does not exist")
 	}
 
 	// Is it a regular file?
 	if !sourceFileStat.Mode().IsRegular() {
-		return 0, errors.New("Not a regular file")
+		log.Fatal("Source file is not a regular file")
 	}
 
 	source, err := os.Open(src)
 	if err != nil {
-		return 0, err
+		log.Fatal(err)
 	}
 	defer source.Close()
 
@@ -30,21 +30,37 @@ func CopyFile(src, dst string) (int64, error) {
 		os.Remove(dst)
 	}
 
+	path, _ := ParsePath(dst)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.MkdirAll(path, 0700)
+	}
+
 	// Create the file
 	destination, err := os.Create(dst)
 	if err != nil {
-		return 0, err
+		log.Fatal(err)
 	}
 	defer destination.Close()
 
-	nBytes, err := io.Copy(destination, source)
-	return nBytes, err
+	_, err = io.Copy(destination, source)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func RemoveFile(path string) {
 	err := os.RemoveAll(path)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func EmptyDir(path string) {
+	if err := os.RemoveAll(path); err != nil {
+		log.Fatal((err))
+	}
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		log.Fatal((err))
 	}
 }
 
@@ -56,13 +72,27 @@ func FileExists(path string) bool {
 }
 
 func IsModified(file1, file2 string) bool {
-	content1, err := os.ReadFile(file1)
+	f1, err := os.Open(file1)
 	if err != nil {
 		log.Fatal(err)
 	}
-	content2, err := os.ReadFile(file2)
+	defer f1.Close()
+	f2, err := os.Open(file2)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return string(content1) != string(content2)
+	defer f2.Close()
+	reader1 := bufio.NewReader(f1)
+	reader2 := bufio.NewReader(f2)
+	for {
+		line1, _, err1 := reader1.ReadLine()
+		line2, _, err2 := reader2.ReadLine()
+		if err1 == io.EOF || err2 == io.EOF {
+			break
+		}
+		if string(line1) != string(line2) {
+			return true
+		}
+	}
+	return false
 }
