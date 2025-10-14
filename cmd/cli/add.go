8df4292b9f1d939e32dd104617ebda72ec75489a
denesbeck <denes.beck1@outlook.com>
2025-10-14 19:51:50 +0200

    test: add

--- cmd/cli/add.go
+++ cmd/cli/add.go
@@ -21,11 +21,11 @@ var addCmd = &cobra.Command{
 	},
 }
 
-func runAddCommand(filePath string) {
+func runAddCommand(filePath string) int {
 	initialized := IsInitialized()
 	if !initialized {
-		color.Red("CSync not initialized")
-		return
+		color.Red(COMMON_RETURN_CODES[99])
+		return 99
 	}
 
 	_, fileName := ParsePath(filePath)
@@ -38,33 +38,35 @@ func runAddCommand(filePath string) {
 		if added {
 			if !exists {
 				removeFileAndLog(id, "added")
-				color.Cyan("File removed from staging")
-				return
+				color.Cyan(ADD_RETURN_CODES[1])
+				return 1
 			}
 			modified := IsModified(filePath, dirs.StagingAdded+id+"/"+fileName)
 			if modified {
 				AddToStaging(id, filePath, "added")
-				color.Cyan("Staged file updated")
-				return
+				color.Cyan(ADD_RETURN_CODES[2])
+				return 2
 			}
-			color.Cyan("File already staged")
-			return
+			color.Cyan(ADD_RETURN_CODES[3])
+			return 3
 		}
 		modified, id, _ := LogEntryLookup("MOD", filePath)
 		if modified {
 			if !exists {
 				removeFileAndLog(id, "modified")
-				stageAndLog(generatedId, filePath, "removed")
-				return
+				LogOperation(generatedId, "REM", filePath)
+				// TODO: check if this is correct!
+				// stageAndLog(generatedId, filePath, "removed")
+				return 4
 			}
 			modified := IsModified(filePath, dirs.StagingModified+id+"/"+fileName)
 			if modified {
 				AddToStaging(id, filePath, "modified")
-				color.Cyan("Staged file updated")
-				return
+				color.Cyan(ADD_RETURN_CODES[5])
+				return 5
 			}
-			color.Cyan("File already staged")
-			return
+			color.Cyan(ADD_RETURN_CODES[6])
+			return 6
 		}
 		removed, id, _ := LogEntryLookup("REM", filePath)
 		if removed {
@@ -72,15 +74,23 @@ func runAddCommand(filePath string) {
 				removeFileAndLog(id, "removed")
 				isCommitted, commitId, fileId := GetFileMetadata(filePath)
 				if !isCommitted {
-					stageAndLog(generatedId, filePath, "added")
+					stageAndLog(generatedId, filePath, "added") // this scenario shouldn't be possible
 				} else {
 					modified := IsModified(filePath, dirs.Commits+commitId+"/"+fileId+"/"+filePath)
 					if modified {
 						stageAndLog(generatedId, filePath, "modified")
+						color.Cyan(ADD_RETURN_CODES[7])
+						return 7
 					}
+					/*
+											* Checking use case when file was not modified is not necessary,
+											* because the file was removed from staging (REM).
+						          * So, everything should be unchanged.
+					*/
 				}
 			} else {
-				color.Cyan("File already staged")
+				color.Cyan(ADD_RETURN_CODES[8])
+				return 8
 			}
 		}
 	} else {
@@ -89,20 +99,26 @@ func runAddCommand(filePath string) {
 		if isDeleted {
 			AddToStaging(generatedId, dirs.Commits+commitId+"/"+fileId+"/"+fileName, "removed")
 			LogOperation(generatedId, "REM", filePath)
-			return
+			color.Cyan(ADD_RETURN_CODES[9])
+			return 9
 		}
 
 		if isCommitted {
 			modified := IsModified(filePath, dirs.Commits+commitId+"/"+fileId+"/"+fileName)
 			if modified {
 				stageAndLog(generatedId, filePath, "modified")
+				return 10
 			} else {
-				color.Red("File not modified")
+				color.Red(ADD_RETURN_CODES[11])
+				return 11
 			}
 		} else {
 			stageAndLog(generatedId, filePath, "added")
+			color.Cyan(ADD_RETURN_CODES[12])
+			return 12
 		}
 	}
+	return 0
 }
 
 func removeFileAndLog(id string, op string) {
