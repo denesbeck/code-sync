f87bab82dae50ed884f2b018489a97bfb191c130
denesbeck <denes.beck1@outlook.com>
2025-10-14 19:51:50 +0200

    refactor: config cmd adj

--- cmd/csync/cli/config.go
+++ cmd/csync/cli/config.go
@@ -11,6 +11,18 @@ import (
 
 func init() {
 	rootCmd.AddCommand(configCmd)
+
+	configCmd.AddCommand(setCmd)
+	configCmd.AddCommand(getCmd)
+
+	setCmd.AddCommand(setDefaultBranchCmd)
+	setCmd.AddCommand(setUsernameCmd)
+	setCmd.AddCommand(setEmailCmd)
+
+	getCmd.AddCommand(getDefaultBranchCmd)
+	getCmd.AddCommand(getUsernameCmd)
+	getCmd.AddCommand(getEmailCmd)
+	getCmd.AddCommand(getUserCmd)
 }
 
 type Config struct {
@@ -18,22 +30,103 @@ type Config struct {
 	Email    string
 }
 
-var configCmd = &cobra.Command{
-	Use:     "config",
-	Short:   "Config manager",
-	Example: "csync config",
+var setCmd = &cobra.Command{
+	Use:   "set",
+	Short: "Set config values",
+	Args:  cobra.ExactArgs(1),
+}
+
+var setDefaultBranchCmd = &cobra.Command{
+	Use:     "default-branch",
+	Short:   "Set default branch",
+	Example: "csync config set default-branch <branch-name>",
+	Args:    cobra.ExactArgs(1),
 	RunE: func(_ *cobra.Command, args []string) error {
-		if args[0] == "set" {
-			setConfig(args[1], args[2])
-			return nil
-		}
-		if args[0] == "get" {
-			getConfig(args[1])
-			return nil
-		}
-		color.Red("Invalid command")
+		setDefaultBranch(args[0])
 		return nil
 	},
+}
+
+var setUsernameCmd = &cobra.Command{
+	Use:     "username",
+	Short:   "Set username",
+	Example: "csync config set username <username>",
+	Args:    cobra.ExactArgs(1),
+	RunE: func(_ *cobra.Command, args []string) error {
+		setConfig("username", args[0])
+		return nil
+	},
+}
+
+var setEmailCmd = &cobra.Command{
+	Use:     "email",
+	Short:   "Set email",
+	Example: "csync config set email <email>",
+	Args:    cobra.ExactArgs(1),
+	RunE: func(_ *cobra.Command, args []string) error {
+		setConfig("email", args[0])
+		return nil
+	},
+}
+
+var getCmd = &cobra.Command{
+	Use:     "get",
+	Short:   "Get config values",
+	Example: "csync config get username <username>",
+	Args:    cobra.ExactArgs(1),
+	RunE: func(_ *cobra.Command, args []string) error {
+		getConfig(args[0])
+		return nil
+	},
+}
+
+var getDefaultBranchCmd = &cobra.Command{
+	Use:     "default-branch",
+	Short:   "Get default branch",
+	Example: "csync config get default-branch",
+	Args:    cobra.ExactArgs(0),
+	Run: func(_ *cobra.Command, args []string) {
+		getDefaultBranch()
+	},
+}
+
+var getUsernameCmd = &cobra.Command{
+	Use:     "username",
+	Short:   "Get username",
+	Example: "csync config get username",
+	Args:    cobra.ExactArgs(0),
+	RunE: func(_ *cobra.Command, args []string) error {
+		getConfig("username")
+		return nil
+	},
+}
+
+var getEmailCmd = &cobra.Command{
+	Use:     "email",
+	Short:   "Get email",
+	Example: "csync config get email <email>",
+	Args:    cobra.ExactArgs(0),
+	RunE: func(_ *cobra.Command, args []string) error {
+		getConfig("email")
+		return nil
+	},
+}
+
+var getUserCmd = &cobra.Command{
+	Use:     "user",
+	Short:   "Get username and email",
+	Example: "csync config get user",
+	Args:    cobra.ExactArgs(0),
+	RunE: func(_ *cobra.Command, args []string) error {
+		getConfig("user")
+		return nil
+	},
+}
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Config manager",
+	Args:  cobra.ExactArgs(1),
 }
 
 func setConfig(key string, value string) {
@@ -81,4 +174,19 @@ func getConfig(key string) {
 	case "user":
 		color.Cyan(config.Username + " <" + config.Email + ">")
 	}
+}
+
+func setDefaultBranch(branch string) {
+	if initialized := IsInitialized(); !initialized {
+		color.Red("CSync not initialized")
+	}
+	SetDefaultBranch(branch)
+}
+
+func getDefaultBranch() {
+	if initialized := IsInitialized(); !initialized {
+		color.Red("CSync not initialized")
+	}
+	branch := GetDefaultBranchName()
+	color.Cyan(branch)
 }
