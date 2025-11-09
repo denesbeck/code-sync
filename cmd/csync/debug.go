1ad92fd0e35f98a94a5593f7608e0cbe2d8ce7a5
denesbeck <denes.beck1@outlook.com>
2025-11-09 19:46:50 +0100

    refactor: interface{} -> any

--- cmd/csync/debug.go
+++ cmd/csync/debug.go
@@ -10,7 +10,7 @@ import (
 var debugLogger = color.New(color.FgBlue).SprintFunc()
 
 // Debug logs a debug message if DEBUG environment variable is set to "true"
-func Debug(format string, args ...interface{}) {
+func Debug(format string, args ...any) {
 	if os.Getenv("DEBUG") == "true" {
 		fmt.Printf("[DEBUG] %s\n", debugLogger(fmt.Sprintf(format, args...)))
 	}
