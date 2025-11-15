ea5041f9e0d849e2fa2f90adea51fcb7e2ef8b1a
denesbeck <denes.beck1@outlook.com>
2025-11-15 19:34:18 +0100

    refactor: new return codes + adj

--- cmd/csync/return_codes.go
+++ cmd/csync/return_codes.go
@@ -4,21 +4,22 @@ var COMMON_RETURN_CODES = map[int]string{
 	001: "CSync not initialized.",
 	002: "Path ignored by one of the rules defined in the rules file.",
 	003: "CSync already initialized.",
+	004: "Invalid path.",
 }
 
 var ADD_RETURN_CODES = map[int]string{
-	101: "File removed from staging.", // file was staged (ADD), but it got removed
-	102: "Staged file updated.",       // file was staged (ADD), but it got modified
-	103: "File already staged.",       // the user staged the same file again (ADD)
-	104: "",                           // file was staged (MOD), but it got removed
-	105: "Staged file updated.",       // file was staged (MOD), but it got modified
-	106: "File already staged.",       // the user staged the same file again (MOD)
-	107: "Staged file update.",        // file was staged (REM), but it got added back and modified
-	108: "File already staged.",       // the user staged the same file again (REM)
-	109: "File added to staging.",     // file committed but not staged -> staged (REM)
-	110: "File added to staging.",     // file committed but not staged -> staged (MOD)
-	111: "File not modified.",         // file committed, not staged, not modified
-	112: "File added to staging.",     // file not committed, not staged -> staged (ADD)
+	101: "File no longer exists, removed from staging.", // file was staged (ADD), but it got removed
+	102: "Staged file updated.",                         // file was staged (ADD), but it got modified
+	103: "File already staged.",                         // the user staged the same file again (ADD)
+	104: "File no longer exists, removed from staging.", // file was staged (MOD), but it got removed
+	105: "Staged file updated.",                         // file was staged (MOD), but it got modified
+	106: "File already staged.",                         // the user staged the same file again (MOD)
+	107: "Staged file updated.",                         // file was staged (REM), but it got added back and modified
+	108: "File already staged.",                         // the user staged the same file again (REM)
+	109: "File added to staging.",                       // file committed but not staged -> staged (REM)
+	110: "File added to staging.",                       // file committed but not staged -> staged (MOD)
+	111: "File not modified.",                           // file committed, not staged, not modified
+	112: "File added to staging.",                       // file not committed, not staged -> staged (ADD)
 }
 
 var BRANCH_RETURN_CODES = map[int]string{
