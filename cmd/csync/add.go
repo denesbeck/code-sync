7d1a823ed53f2e624f259420f5bd22f485438bf3
denesbeck <denes.beck1@outlook.com>
2025-11-15 19:34:50 +0100

    refactor: ui improvement -- add

--- cmd/csync/add.go
+++ cmd/csync/add.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -30,20 +29,20 @@ func runAddCommand(filePath string, force bool) int {
 	Debug("Processing file: %s", filePath)
 	initialized := IsInitialized()
 	if !initialized {
-		color.Red(COMMON_RETURN_CODES[001])
+		Fail(COMMON_RETURN_CODES[001])
 		return 001
 	}
 
-	// Validate path to prevent traversal attacks
 	if err := ValidatePath(filePath); err != nil {
-		color.Red("Invalid file path: " + err.Error())
-		return 001
+		Debug("Path is invalid: %s", err.Error())
+		Fail(COMMON_RETURN_CODES[004])
+		return 004
 	}
 
 	if !force {
 		shouldIgnore := ShouldIgnore(filePath)
 		if shouldIgnore {
-			color.Red(COMMON_RETURN_CODES[002])
+			Warning(COMMON_RETURN_CODES[002])
 			return 002
 		}
 	}
@@ -61,28 +60,28 @@ func runAddCommand(filePath string, force bool) int {
 			if !exists {
 				Debug("File was added but no longer exists, removing from staging")
 				if err := removeFileAndLog(id, "added"); err != nil {
-					color.Red("Error removing file from staging: " + err.Error())
-					return 001
+					Debug("Error removing file from staging: %s", err.Error())
+					MustSucceed(err, "operation failed")
 				}
-				color.Cyan(ADD_RETURN_CODES[101])
+				Info(ADD_RETURN_CODES[101])
 				return 101
 			}
 			modified, err := IsModified(filePath, dirs.StagingAdded+id+"/"+fileName)
 			if err != nil {
-				color.Red("Error checking if file is modified: " + err.Error())
-				return 001
+				Debug("Error checking if file is modified: %s", err.Error())
+				MustSucceed(err, "operation failed")
 			}
 			if modified {
 				Debug("File was added and modified, updating staging")
 				if err := AddToStaging(id, filePath, "added"); err != nil {
-					color.Red("Error adding file to staging: " + err.Error())
-					return 001
+					Debug("Error adding file to staging: %s", err.Error())
+					MustSucceed(err, "operation failed")
 				}
-				color.Cyan(ADD_RETURN_CODES[102])
+				Success(ADD_RETURN_CODES[102])
 				return 102
 			}
 			Debug("File was added but not modified")
-			color.Cyan(ADD_RETURN_CODES[103])
+			Info(ADD_RETURN_CODES[103])
 			return 103
 		}
 		modified, id, _ := LogEntryLookup("MOD", filePath)
@@ -90,28 +89,29 @@ func runAddCommand(filePath string, force bool) int {
 			if !exists {
 				Debug("File was modified but no longer exists, removing from staging")
 				if err := removeFileAndLog(id, "modified"); err != nil {
-					color.Red("Error removing file from staging: " + err.Error())
-					return 001
+					Debug("Error removing file from staging: %s", err.Error())
+					MustSucceed(err, "operation failed")
 				}
 				LogOperation(generatedId, "REM", filePath)
+				Info(ADD_RETURN_CODES[104])
 				return 104
 			}
 			modified, err := IsModified(filePath, dirs.StagingModified+id+"/"+fileName)
 			if err != nil {
-				color.Red("Error checking if file is modified: " + err.Error())
-				return 001
+				Debug("Error checking if file is modified: %s", err.Error())
+				MustSucceed(err, "operation failed")
 			}
 			if modified {
 				Debug("File was modified and changed, updating staging")
 				if err := AddToStaging(id, filePath, "modified"); err != nil {
-					color.Red("Error adding file to staging: " + err.Error())
-					return 001
+					Debug("Error adding file to staging: %s", err.Error())
+					MustSucceed(err, "operation failed")
 				}
-				color.Green(ADD_RETURN_CODES[105])
+				Success(ADD_RETURN_CODES[105])
 				return 105
 			}
 			Debug("File was modified but not changed")
-			color.Cyan(ADD_RETURN_CODES[106])
+			Info(ADD_RETURN_CODES[106])
 			return 106
 		}
 		removed, id, _ := LogEntryLookup("REM", filePath)
@@ -119,27 +119,27 @@ func runAddCommand(filePath string, force bool) int {
 			if exists {
 				Debug("File was removed but exists again, checking modifications")
 				if err := removeFileAndLog(id, "removed"); err != nil {
-					color.Red("Error removing file from staging: " + err.Error())
-					return 001
+					Debug("Error removing file from staging: %s", err.Error())
+					MustSucceed(err, "operation failed")
 				}
 				_, commitId, fileId := GetFileMetadata(filePath)
 				modified, err := IsModified(filePath, dirs.Commits+commitId+"/"+fileId+"/"+fileName)
 				if err != nil {
-					color.Red("Error checking if file is modified: " + err.Error())
-					return 001
+					Debug("Error checking if file is modified: %s", err.Error())
+					MustSucceed(err, "operation failed")
 				}
 				if modified {
 					Debug("File was removed but modified, adding back as modified")
 					if err := stageAndLog(generatedId, filePath, "modified"); err != nil {
-						color.Red("Error staging file: " + err.Error())
-						return 001
+						Debug("Error staging file: %s", err.Error())
+						MustSucceed(err, "operation failed")
 					}
-					color.Cyan(ADD_RETURN_CODES[107])
+					Success(ADD_RETURN_CODES[107])
 					return 107
 				}
 			} else {
 				Debug("File was removed and still doesn't exist")
-				color.Cyan(ADD_RETURN_CODES[108])
+				Info(ADD_RETURN_CODES[108])
 				return 108
 			}
 		}
@@ -150,40 +150,40 @@ func runAddCommand(filePath string, force bool) int {
 		if isDeleted {
 			Debug("File was committed but deleted, staging for removal")
 			if err := AddToStaging(generatedId, dirs.Commits+commitId+"/"+fileId+"/"+fileName, "removed"); err != nil {
-				color.Red("Error adding file to staging: " + err.Error())
-				return 001
+				Debug("Error adding file to staging: %s", err.Error())
+				MustSucceed(err, "operation failed")
 			}
 			LogOperation(generatedId, "REM", filePath)
-			color.Green(ADD_RETURN_CODES[109])
+			Success(ADD_RETURN_CODES[109])
 			return 109
 		}
 
 		if isCommitted {
 			modified, err := IsModified(filePath, dirs.Commits+commitId+"/"+fileId+"/"+fileName)
 			if err != nil {
-				color.Red("Error checking if file is modified: " + err.Error())
-				return 001
+				Debug("Error checking if file is modified: %s", err.Error())
+				MustSucceed(err, "operation failed")
 			}
 			if modified {
 				Debug("File was committed and modified, staging as modified")
 				if err := stageAndLog(generatedId, filePath, "modified"); err != nil {
-					color.Red("Error staging file: " + err.Error())
-					return 001
+					Debug("Error staging file: %s", err.Error())
+					MustSucceed(err, "operation failed")
 				}
-				color.Green(ADD_RETURN_CODES[110])
+				Success(ADD_RETURN_CODES[110])
 				return 110
 			} else {
 				Debug("File was committed but not modified")
-				color.Red(ADD_RETURN_CODES[111])
+				Info(ADD_RETURN_CODES[111])
 				return 111
 			}
 		} else {
 			Debug("File is new, staging as added")
 			if err := stageAndLog(generatedId, filePath, "added"); err != nil {
-				color.Red("Error staging file: " + err.Error())
-				return 001
+				Debug("Error staging file: %s", err.Error())
+				MustSucceed(err, "operation failed")
 			}
-			color.Green(ADD_RETURN_CODES[112])
+			Success(ADD_RETURN_CODES[112])
 			return 112
 		}
 	}
